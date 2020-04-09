fd24d7630cbd88e6249da2a0c79de68f8a506810
bashshadrack <bash.shadrack@gmail.com>
2020-04-09 19:52:02 +0200

    cleaned develop

--- attendance-list/src/App.js
+++ attendance-list/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import SignUp from './component/SignUp';
-// import StartingDate from "./component/StartingDate";
 
 function App() {
   return (
     <div className="App">
       <SignUp/>
-      {/* <StartingDate /> */}
     </div>
   );
 }
